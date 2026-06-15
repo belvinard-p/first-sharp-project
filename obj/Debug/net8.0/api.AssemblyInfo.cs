5bab49cc26691caeecf933647975b5b80f8ec0cc
belvip <pouadjeubelvi@gmail.com>
2026-06-15 12:46:32 +0000

    add migration

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da6e87b2045cbeb875c13dff0b6b65a76675ea8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72a31952ba168c3f16749cad0fccb50098e82861")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
